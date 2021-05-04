9c67e62895f431d0470f41de575cbfa5dce205a1
marvin255 <postbox433@gmail.com>
2021-05-04 20:47:31 +0200

    Add basic bundle files (#1)

---
+++ Marvin255JwtSymfonyBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\Jwt\Symfony;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle object.
+ */
+class Marvin255JwtSymfonyBundle extends Bundle
+{
+}
